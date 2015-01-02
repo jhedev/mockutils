20e01e90c5b6058e57192327d3d5f2eb0cfc6da1
Joel Hermanns <joel.hermanns@gmail.com>
2015-01-02 15:12:59 +0100

    Inital commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mockutils',
+      version='0.0.1',
+      description='Utils for the unittest.mock module',
+      url='http://github.com/jhedev/mockutils',
+      author='Joel Hermanns',
+      author_email='joel.hermanns@gmail.com',
+      license='MIT',
+      packages=['mockutils'],
+      zip_safe=False)
